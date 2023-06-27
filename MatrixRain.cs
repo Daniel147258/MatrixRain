081be5841ccf9cfe2b33c77d6ad2a93d7d715d90
Daniel147258 <105059165+Daniel147258@users.noreply.github.com>
2023-06-27 21:47:26 +0200

    commit

---
+++ MatrixRain.cs
@@ -0,0 +1,158 @@
+﻿using MatrixRain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixRain;
+
+class MatrixRain
+{
+    private char[] znaky;
+    private Character rozmer;
+    private int poziciaRiadku;
+    private bool directionUp;
+    private int poziciaStlpca;
+    private bool skoncil;
+    private int dlzka;
+    private Enum color;
+    public MatrixRain(bool directionUp, Enum color, Character rozmerNadobudanychHodnot)
+    {
+        Random rand = new Random();
+        this.directionUp = directionUp;
+        this.rozmer = rozmerNadobudanychHodnot;
+        this.color = color;
+        this.poziciaRiadku = 0;
+        if (this.directionUp) 
+            this.poziciaRiadku = Console.WindowHeight - 1;
+
+        this.poziciaStlpca = rand.Next(0, Console.WindowWidth);
+        this.dlzka = rand.Next(3, Console.WindowHeight);
+        this.znaky = new char[this.dlzka];
+        this.skoncil = false;
+    }
+    public void NaplnPole()
+    {
+        this.znaky = new char[this.dlzka];
+        Random rand = new Random();
+        for (int i = 0; i < this.znaky.Length; i++)
+        {
+            int hodnota = 0;
+            switch (this.rozmer)
+            {
+                case Character.AlphaNumeric:
+                    hodnota = rand.Next(48, 123);
+                    while ((hodnota >= 58 && hodnota <= 64) || (hodnota >= 91 && hodnota <= 96))
+                    {
+                        hodnota = rand.Next(48, 123);
+                    }
+                    this.znaky[i] = (char)hodnota;
+                    break;
+                case Character.Alpha:
+                    hodnota = rand.Next(65, 123);
+                    while ((hodnota >= 91 && hodnota <= 96))
+                    {
+                        hodnota = rand.Next(65, 123);
+                    }
+                    this.znaky[i] = (char)hodnota;
+                    break;
+                case Character.Numeric:
+                    this.znaky[i] = (char)rand.Next(48, 58);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void Vykresli()
+    {
+        int pocitadlo = 0;
+        Console.ForegroundColor = (ConsoleColor)this.color;
+        if (!this.directionUp)
+        {
+            this.NaplnPole();
+            foreach (char ch in this.znaky)
+            {
+                if (pocitadlo + 1 == this.znaky.Length)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                if (this.poziciaRiadku + pocitadlo < Console.WindowHeight - 1)
+                {
+                    Console.SetCursorPosition(this.poziciaStlpca, this.poziciaRiadku + pocitadlo);
+                    Console.WriteLine(ch);
+                    Console.ForegroundColor = (ConsoleColor)this.color;
+                    pocitadlo++;
+                }
+                else
+                {
+                    this.dlzka--;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            this.NaplnPole();
+            foreach (char ch in this.znaky)
+            {
+                if (pocitadlo + 1 == this.znaky.Length)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                if (this.poziciaRiadku - pocitadlo > -1)
+                {
+                    Console.SetCursorPosition(this.poziciaStlpca, this.poziciaRiadku - pocitadlo);
+                    Console.WriteLine(ch);
+                    Console.ForegroundColor = (ConsoleColor)this.color;
+                    pocitadlo++;
+                }
+                else
+                {
+                    this.dlzka--;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void SimulujPad()
+    {
+        if (!this.directionUp)
+        {
+            if(this.poziciaRiadku >= Console.WindowHeight)
+            {
+                this.skoncil = true;
+            }
+                this.Vykresli();
+            if (this.poziciaRiadku < Console.WindowHeight)
+            {
+                Console.SetCursorPosition(this.poziciaStlpca, this.poziciaRiadku);
+                Console.Write(" ");
+            }
+            this.poziciaRiadku++;
+        }
+        else
+        {
+            if (this.poziciaRiadku < 0)
+            {
+                this.skoncil = true;
+            }
+            this.Vykresli();
+            if (this.poziciaRiadku > -1)
+            {
+                Console.SetCursorPosition(this.poziciaStlpca, this.poziciaRiadku);
+                Console.Write(" ");
+            }
+            this.poziciaRiadku--;
+
+        }
+    }
+
+    public bool GetSkoncil()
+    {
+        return this.skoncil;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using MatrixRain;
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        // prezentuje help
+        if (args[4] != null && args[4] == "help")
+        {
+            Console.WriteLine("1: directionUp(PRVY PARAMETER) – určuje smer, či bude kód „padať“ smerom " +
+                "zdola nahor („opačná gravitácia“).\n");
+            Console.WriteLine("2: color(DRUHY PARAMETER) – farba vykresľovaných znakov typu ConsoleColor\n");
+            Console.WriteLine("3: delay-speed(TRETI PARAMETER) – spomalenie rýchlosti vykresľovania kódu " +
+                "v type celého čísla. Určuje počet milisekúnd\n");
+            Console.WriteLine("4: characters – určuje množinu znakov, z ktorých sa budú generovať znaky padajúcich kódov. " +
+                "Možné hodnoty budú:\r\no Alpha – budú sa generovať abecedné (alfa) znaky od ‘A’ po ‘Z’," +
+                "\r\no Numeric – budú sa generovať numerické znaky od ‘0’ po ‘9’," +
+                "\r\no AlphaNumeric – budú sa generovať alfanumerické znaky od ‘0’ (nula) po ‘Z’ (písmeno Z).\n");
+            Console.WriteLine("Program sa dá ukončiť stiskom ľubovoľnej klávesnice");
+            System.Threading.Thread.Sleep(10000);
+            Console.Clear();
+        }
+
+        Console.CursorVisible = false;
+        bool ok = true;
+        List<MatrixRain.MatrixRain> list = new List<MatrixRain.MatrixRain>();
+        int delaySpeed = 1;
+        bool directionUp = false;
+
+        // slúži na zisťovanie parametra direction-up
+        if (args[0] == null)
+        {
+            Console.WriteLine("Argument príkazu smeru pohybu sa nepodarilo " +
+                "načitať preto bude použitá prednastavená hodnota");
+            ok = false;
+        }
+        else
+        {
+            switch(args[0])
+            {
+                case "true":
+                    directionUp = true;
+                    break;
+                case "false":
+                    break;
+                default:
+                    Console.WriteLine("Argument príkazu smeru pohybu sa nepodarilo " +
+                "načitať preto bude použitá prednastavená hodnota");
+                    ok = false;
+                    break;
+
+            }
+        }
+
+        // slúži na zisťovanie parametra delaySpeed
+        if (args[1] != null)
+        {
+            try
+            {
+                delaySpeed = Math.Abs(int.Parse(args[1]));
+            }
+            catch
+            {
+                Console.WriteLine("Zadaný parameter delaySpeed sa nepodarilo skonvertovať na celé číslo preto sa použije" +
+                    " prednastavená rýchlosť");
+                ok  = false;
+            }
+        }
+
+        // slúži na zisťovanie parametra color
+        if (!Enum.TryParse(args[2], true, out ConsoleColor foregroundColor) || args[2] == null)
+        {
+            Console.WriteLine("Nepodarilo sa získať farbu preto sa použije prednastavená farba");
+            foregroundColor = ConsoleColor.Green;
+            ok = false;
+        }
+
+        // slúži na zisťovanie parametra Character
+        if (!Enum.TryParse(args[3], true, out Character character) || args[2] == null)
+        {
+            Console.WriteLine("Nepodarilo sa získať interval znakov preto sa použije prednastavený interval");
+            character = Character.AlphaNumeric;
+            ok = false;
+        }
+
+        // slúži ako výstup pre použivaťeľa, keď bol parameter zle zadaný tak sa výpíše
+        if (!ok) 
+        {
+            System.Threading.Thread.Sleep(5000);
+        }
+        Console.Clear();
+
+        // tu je už kod ktorý prezentuje "MatrixRain"
+        while (!Console.KeyAvailable)
+        {  
+            MatrixRain.MatrixRain r = new MatrixRain.MatrixRain(directionUp, foregroundColor, character);
+            if (list.Count <= 36)
+            {
+                list.Add(r);
+            }
+            foreach (var a in list)
+            {
+                if (a.GetSkoncil())
+                {
+                    list.Remove(a);
+                    break;
+                }
+                else
+                {
+                    a.SimulujPad();
+                }
+            }
+            System.Threading.Thread.Sleep(delaySpeed);
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Clear();
+        Console.CursorVisible = true;
+    }
+}
+
